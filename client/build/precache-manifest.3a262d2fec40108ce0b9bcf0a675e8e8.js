d27f92d36c39d0d4d94859d2b4a2d83babab35ac
Paul Yoon <yoonpaul12@gmail.com>
2020-03-20 18:19:05 -0700

    Mern exam deployment

---
+++ client/build/precache-manifest.3a262d2fec40108ce0b9bcf0a675e8e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443789c50b754336a167539eb10c5194",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8edc82d65756828d4014",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ff0f8956a30d714d1319",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8edc82d65756828d4014",
+    "url": "/static/js/2.267e8581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.267e8581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0f8956a30d714d1319",
+    "url": "/static/js/main.0fe00cc8.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
